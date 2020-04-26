139d69168bfa2053aae9f0fcef3df401ee0d65f2
Adrodoc <adrodoc55@googlemail.com>
2020-04-26 18:34:46 +0200

    Add Cats

--- app/src/main/java/com/example/randomcats/MainActivity.kt
+++ app/src/main/java/com/example/randomcats/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.example.randomcats
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.squareup.picasso.MemoryPolicy
+import com.squareup.picasso.NetworkPolicy
+import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val nextCat = findViewById<Button>(R.id.nextCat)
+        val catImage = findViewById<ImageView>(R.id.catImage)
+        val url = "https://loremflickr.com/1080/720/cat"
+        nextCat.setOnClickListener {
+            Picasso.get()
+                .load(url)
+                .networkPolicy(NetworkPolicy.NO_CACHE)
+                .memoryPolicy(MemoryPolicy.NO_CACHE)
+                .into(catImage)
+        }
     }
 }
